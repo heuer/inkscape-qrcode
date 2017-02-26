38ca792df6c7fe8c2c1a77e003b073f530b910d0
Lars <heuer@semagia.com>
2017-02-26 21:05:39 +0100

    Updated Segno to version 0.2.5

--- inkscape_qrcode/_segno/__init__.py
+++ inkscape_qrcode/_segno/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2016 -- Lars Heuer - Semagia <http://www.semagia.com/>.
+# Copyright (c) 2016 - 2017 -- Lars Heuer - Semagia <http://www.semagia.com/>.
 # All rights reserved.
 #
 # License: BSD License
 #
 
-__version__ = '0.2.4'
+__version__ = '0.2.5'
